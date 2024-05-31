0e667505978a8ea881b2d63d51c10ebf1765370b
arihantj916 <arihantj916@gmail.com>
2024-05-31 06:47:07 +0000

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="text-blue-500">
+    <main className="text-blue-500 text-center text-3xl">
       Hello This is done from Google Idx
     </main>
   );
